553de504f0fffecd390f7c2a284bd1eea43eb478
idiot2222 <pky2892@gmail.com>
2022-01-12 15:58:30 +0900

    fix: index.html 위치 변경에 따른 webpack 설정 수정

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
     mode: "development",
-    entry: "./index.jsx",
+    entry: "/src/index.jsx",
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: "bundle.js"
